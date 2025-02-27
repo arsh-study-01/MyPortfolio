49ad2a23067dba0bc4cadc52c381f91b4173c964
Arshnoor <study.arshnoorkirmani@gmail.com>
2025-02-27 20:56:48 +0530

    responseve

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function myInfoShow() {
   let elm = document.querySelector(".myInfo-container");
   elm.classList.toggle("showInfo");
   if (elm.classList[1] == "showInfo")
-    document.querySelector(".show-conatacts").innerHTML = "Hide Contacts";
-  else document.querySelector(".show-conatacts").innerHTML = "Show Contacts";
+    document.querySelector(".show-conatacts span.text").innerHTML =
+      "Hide Contacts";
+  else
+    document.querySelector(".show-conatacts span.text").innerHTML =
+      "Show Contacts";
 }
 let Nav_Btns_Box = document.querySelector("nav>ul");
 console.log(Nav_Btns_Box);
